f9e0cea28ef8031ec93d5f33e0d239c0bf041ba2
Subham <subhamkumarr456@gmail.com>
2022-01-02 06:38:07 -0500

    base route added

--- router/emailservice.js
+++ router/emailservice.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const {verifyEmail}=require("../controller/verifyemail");
 const {resetPassword}=require("../controller/resetPassword");
 
-
+router.get('/',(req,res)=>{
+    res.send("Email Service");
+})
 router.post('/verifyemail',verifyEmail);
 
 router.post('/resetpassword',resetPassword);
